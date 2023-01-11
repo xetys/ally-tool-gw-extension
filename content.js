0295298329cbac1c6feb2b0461414752cb4c3b99
David Steiman <d.steiman@syseleven.de>
2023-01-11 14:44:08 +0100

    changed towards using ajax instead of navigation inside angular

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+let titleAnimation = null;
 function changePage(url) {
     window.history.pushState("info", "info" , "https://ally-tool.sys11.stytex.cloud" + url); 
     location.hash = ' ';
@@ -7,12 +8,47 @@ function changePage(url) {
     }, 100)
 }
 
+function animateTitle() {
+    if (!titleAnimation) { 
+        const currentTitle = document.title;
+        document.title = "tik tok";
+
+
+        titleAnimation = setTimeout(function () {
+            titleAnimation = null;
+            document.title = currentTitle;
+        }, 499)
+    }
+}
+
+function postData(url, input) {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        var token = localStorage.getItem('jhi-authenticationtoken').replaceAll(/["]/g, "");
+
+        xhr.open("POST", url);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(xhr.response);
+                } else {
+                    reject(xhr.statusText);
+                }
+            }
+        };
+        var data = JSON.stringify({text: input});
+        xhr.send(data);
+    });
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+        console.log('got message from worker', sender, request);
         switch (request.action) {
             case "execute_script": {
                 // your code here
-                console.log('got message from worker', sender, sendResponse);
                 document.execCommand("selectAll"); document.execCommand("copy");
                 window.getSelection().removeAllRanges();
                 var ta = document.createElement('textarea');
@@ -28,20 +64,38 @@ chrome.runtime.onMessage.addListener(
                 if (location.href.includes('ally-tool')) {
                     switch (request.dataType) {
                         case "highscore":
-                            changePage("/import");
-                            setTimeout(function () {
-                                document.querySelector('body > jhi-main > div.container-fluid > div > jhi-stat-import > div > form > textarea').focus();
-                                document.execCommand('paste');
-                                document.querySelector('body > jhi-main > div.container-fluid > div > jhi-stat-import > div > form > button').click();
-                            }, 100);
+                            // changePage("/import");
+                            // setTimeout(function () {
+                            //     document.querySelector('body > jhi-main > div.container-fluid > div > jhi-stat-import > div > form > textarea').focus();
+                            //     document.execCommand('paste');
+                            //     document.querySelector('body > jhi-main > div.container-fluid > div > jhi-stat-import > div > form > button').click();
+                            // }, 100);
+                            postData("https://ally-tool.sys11.stytex.cloud/api/v1/import", request.data)
+                                .then(response => {
+                                    console.log("highscore import succeeded");
+                                    animateTitle();
+                                    sendResponse(response);
+                                }).catch(e => {
+                                    console.error('an error occured', e);
+                                    sendResponse(e);
+                                });
                             break;
                         case "info":
-                            changePage("/internal/player-info-import");
-                            setTimeout(function () {
-                                document.querySelector('body > jhi-main > div.container-fluid > div > jhi-player-info-import > div > form > textarea').focus();
-                                document.execCommand('paste');
-                                // document.querySelector('body > jhi-main > div.container-fluid > div > jhi-player-info-import > div > form > button').click();
-                            }, 100);
+                            postData("https://ally-tool.sys11.stytex.cloud/api/v1/player-info/import", request.data)
+                                .then(response => {
+                                    console.log("player info import succeeded");
+                                    animateTitle();
+                                    sendResponse(response);
+                                }).catch(e => {
+                                    console.error('an error occured', e);
+                                    sendResponse(e);
+                                });
+                            // changePage("/internal/player-info-import");
+                            // setTimeout(function () {
+                            //     document.querySelector('body > jhi-main > div.container-fluid > div > jhi-player-info-import > div > form > textarea').focus();
+                            //     document.execCommand('paste');
+                            //     // document.querySelector('body > jhi-main > div.container-fluid > div > jhi-player-info-import > div > form > button').click();
+                            // }, 100);
                     }
                 }
                 sendResponse();
